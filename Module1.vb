8ab044593e227f7b64f65a120f1117587005c146
Gloria Zheng <G.Zheng@ampleforth.org.uk>
2020-08-05 19:38:06 +0100

    Add project files.

---
+++ Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim FullName, surnameStarts, numChars, initial, surname As String
+
+        Console.WriteLine("Please enter your first name and surname, seperated by space: ")
+
+        FullName = Console.ReadLine()
+
+        surnameStarts = FullName.IndexOf(" ")
+
+        surnameStarts = surnameStarts + 1
+
+        numChars = FullName.Length()
+
+        initial = FullName.Substring(0, 1)
+
+        surname = FullName.Substring(surnameStarts)
+
+        Console.WriteLine(initial & " " & surname)
+
+        Console.Read()
+
+    End Sub
+
+End Module
